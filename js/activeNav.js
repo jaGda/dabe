cdab8273c5cf59b2fd897ac428524e499c1051b5
jagda <danielzychniewicz@wp.pl>
2019-03-27 22:42:54 +0100

    memo-game-added

--- js/activeNav.js
+++ js/activeNav.js
@@ -1,11 +1,10 @@
 let mainNavLinks = document.querySelectorAll('nav ul li a');
+mainNavLinks = [...mainNavLinks];
 
 window.addEventListener("scroll", () => {
-    let fromTop = window.scrollY;
-
     mainNavLinks.forEach(a => {
         let section = document.querySelector(a.hash);
-        if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+        if (section.offsetTop - 75 <= window.scrollY && section.offsetTop - 75 + section.offsetHeight > window.scrollY) {
             a.classList.add("active");
         } else {
             a.classList.remove("active");
