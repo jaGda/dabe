d78e096923c3135b9520f9d2f327507f9c878601
jagda <danielzychniewicz@wp.pl>
2019-05-12 18:36:10 +0200

    slider.js updated

--- js/slider.js
+++ js/slider.js
@@ -1,27 +1,23 @@
+const slides = [...document.getElementsByClassName("mySlides")];
+const dots = [...document.getElementsByClassName("dots__span")];
 let slideIndexAuto = 0;
-showSlidesAutomatic();
+let slideIndex = 1;
+
+const hideSlide = slide => {
+  slide.style.display = "none";
+};
+
+const disactivateDot = dot => {
+  dot.className = dot.className.replace("active", "");
+};
 
 function showSlidesAutomatic() {
-  let slides = document.getElementsByClassName("mySlides");
-  slides = [...slides];
-  let dots = document.getElementsByClassName("dots__span");
-  dots = [...dots];
-
-  const hideSlide = slide => {
-    slide.style.display = "none";
-  };
-
   slides.forEach(hideSlide);
-
   slideIndexAuto++;
 
   if (slideIndexAuto > slides.length) {
     slideIndexAuto = 1;
   }
-
-  disactivateDot = dot => {
-    dot.className = dot.className.replace("active", "");
-  };
 
   dots.forEach(disactivateDot);
 
@@ -30,8 +26,19 @@ function showSlidesAutomatic() {
   setTimeout(showSlidesAutomatic, 3000);
 }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+function showSlides(n) {
+  if (n > slides.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides.forEach(hideSlide);
+  dots.forEach(disactivateDot);
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("active");
+}
 
 function plusSlides(n) {
   showSlides((slideIndex += n));
@@ -41,30 +48,4 @@ function currentSlide(n) {
   showSlides((slideIndex = n));
 }
 
-function goToFirstSlide() {
-  slideIndex = 1;
-}
-
-function goToLastSlide() {
-  slideIndex = slides.length;
-}
-
-function showSlides(n) {
-  const isLastSlide = n > slides.length;
-  // getElementsByClassName returns NodeElements, need to convert in to array
-  const slides = [...document.getElementsByClassName("mySlides")];
-  const dots = [...document.getElementsByClassName("dots__span")];
-
-  if (isLastSlide) {
-    goToFirstSlide();
-  } else if (n < 1) {
-    goToLastSlide();
-  }
-
-  slides.forEach(hideSlide);
-
-  dots.forEach(disactivateDot);
-
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].classList.add("active");
-}
+showSlidesAutomatic();
