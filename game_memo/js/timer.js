8a740b8ea1e117296594c066ebbcafa828af8b8b
jagda <danielzychniewicz@wp.pl>
2019-05-18 11:08:41 +0200

    rename-css-files

--- game_memo/js/timer.js
+++ game_memo/js/timer.js
@@ -1,20 +1,20 @@
 class Timer {
-    constructor(timer) {
-        this.timer = timer;
-        this.gameTime = 0;
-        this.interval = null;
-    }
+  constructor(timer) {
+    this.timer = timer;
+    this.gameTime = 0;
+    this.interval = null;
+  }
 
-    start() {
-        this.interval = setInterval(() => {
-            this.gameTime += 0.01;
-            this.timer.innerHTML = this.gameTime.toFixed(2);
-        }, 10);
-    }
+  start() {
+    this.interval = setInterval(() => {
+      this.gameTime += 0.01;
+      this.timer.innerHTML = this.gameTime.toFixed(2);
+    }, 10);
+  }
 
-    stop() {
-        clearInterval(this.interval);
-    }
+  stop() {
+    clearInterval(this.interval);
+  }
 }
 
 const timer = new Timer(document.getElementById("timer"));
